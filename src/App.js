d1ad5c94c5957d75399dfb417e6261a24bb00c8c
franjmr0416 <franjmr16@gmail.com>
2022-10-11 23:38:40 -0500

    rutas anidadas

--- src/App.js
+++ src/App.js
@@ -1,4 +1,26 @@
-import { Routes, Route, Link } from "react-router-dom";
+import { Routes, Route, Link } from 'react-router-dom';
+
+const Portafolio = () => {
+  return (
+    <div>
+      <h1>Portafolio</h1>
+      <ul>
+        <li>
+          <Link to={'/portafolio/proyecto-1'}>Proyecto 1</Link>
+        </li>
+        <li>
+          <Link to={'/portafolio/proyecto-2'}>Proyecto 2</Link>
+        </li>
+      </ul>
+      <div>
+        <Routes>
+          <Route path={'proyecto-1'} element={<h2>Proyecto 1</h2>} />
+          <Route path={'proyecto-2'} element={<h2>Proyecto 2</h2>} />
+        </Routes>
+      </div>
+    </div>
+  );
+};
 
 function App() {
   return (
@@ -16,7 +38,7 @@ function App() {
       <section>
         <Routes>
           <Route exact path="/" element={<h1>Inicio</h1>} />
-          <Route path="/portafolio" element={<h1>Portafolio</h1>} />
+          <Route path="/portafolio/*" element={<Portafolio />} />
         </Routes>
       </section>
     </div>
